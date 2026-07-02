6deb646013c0df0ab32424a152586334142faf8b
LiuQi <liu67iq@gmail.com>
2026-07-02 13:37:23 +0800

    什么周期实践

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,61 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './utils.js';
+import { handleFilePath, handleUrl } from './utils/index.js';
 
 let mainWindow: BrowserWindow;
 
+app.on('will-finish-launching', () => {
+  console.log(0, 'will-finish-launching');
+  app.setName('LQ');
+});
+
+app.on('before-quit', (event) => {
+  console.log('before-quit');
+});
+
+app.on('will-quit', (event) => {
+  console.log('will-quit');
+});
+
+app.on('quit', (event) => {
+  console.log('quit');
+});
+
+app.on('window-all-closed', () => {
+  console.log('window-all-closed 所有窗口都已被关闭');
+  app.quit();
+});
+
+app.on('open-file', (event, filePath) => {
+  event.preventDefault();
+  handleFilePath(filePath);
+});
+
+app.on('open-url', (event, url) => {
+  event.preventDefault();
+  handleUrl(url);
+});
+
+if (app.requestSingleInstanceLock()) {
+  app.on('second-instance', (event, argv) => {
+    console.log('second-instance', argv);
+    // 找到参数中包含文件路径的那一项
+    const filePath = argv.find((arg) => arg.endsWith('.txt'));
+    if (filePath) handleFilePath(filePath);
+
+    const url = argv.find((arg) => arg.startsWith('myapp://'));
+    if (url) handleUrl(url);
+  });
+}
+
 app.on('ready', () => {
+  console.log(1, 'ready');
+  console.log(2, app.getName());
+});
+
+app.whenReady().then(() => {
+  console.log('whenReady');
   mainWindow = new BrowserWindow({});
 
   if (isDev()) {
---
+++ src/app/utils/index.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+export const handleFilePath = (filePath: string) => {
+  if (!filePath) return;
+
+  // 使用 path.resolve 将其强制转换为绝对路径
+  // 如果输入已经是绝对路径，它不会有任何副作用
+  const absolutePath = path.resolve(filePath);
+
+  console.log('处理后的绝对路径:', absolutePath);
+  // 这里调用你之前的 handleOpenFile 逻辑
+};
+
+export const handleUrl = (url: string) => {
+  console.log('接收到 URL:', url);
+};
